15ad0ed9b7660815c3fb0e3e7060a25a358df3b6
Marek Kaszycki <marek.kaszycki@northmill.se>
2021-07-12 13:47:38 +0200

    Small improvement in SplashFragment

--- app/src/main/java/com/mksoftware101/notes/splash/SplashFragment.kt
+++ app/src/main/java/com/mksoftware101/notes/splash/SplashFragment.kt
@@ -1,13 +1,21 @@
 package com.mksoftware101.notes.splash
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import android.view.*
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.WindowManager
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.mksoftware101.notes.R
 
 class SplashFragment : Fragment() {
+
+    companion object {
+        const val DELAY_MS = 4000L
+    }
+
     private lateinit var viewModel: SplashViewModel
 
     override fun onCreateView(
@@ -18,21 +26,25 @@ class SplashFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_splash, container, false)
     }
 
-    private fun setFullScreenMode() =
-        requireActivity().window.addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         view.postDelayed(object : Runnable {
             override fun run() {
-                val direction = SplashFragmentDirections.actionSplashFragmentToHomeFragment()
-                findNavController().navigate(direction)
+                navigateHomeScreen()
             }
-        }, 4000)
+        }, DELAY_MS)
+    }
+
+    private fun navigateHomeScreen() {
+        val direction = SplashFragmentDirections.actionSplashFragmentToHomeFragment()
+        findNavController().navigate(direction)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(SplashViewModel::class.java)
     }
+
+    private fun setFullScreenMode() =
+        requireActivity().window.addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
 }
