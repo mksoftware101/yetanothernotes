55d46575c7a544c77ffa2dea087ed11a1075a312
Marek Kaszycki <marek.kaszycki@northmill.se>
2021-11-04 19:36:53 +0100

    Changed schema in Db model; added extensions

--- app/src/main/java/com/mksoftware101/notes/features/allnotes/domain/FetchNotesUseCase.kt
+++ app/src/main/java/com/mksoftware101/notes/features/allnotes/domain/FetchNotesUseCase.kt
@@ -2,10 +2,11 @@ package com.mksoftware101.notes.features.allnotes.domain
 
 import com.mksoftware101.notes.core.repository.NotesRepository
 import com.mksoftware101.notes.core.usecase.NoParamUseCase
+import com.mksoftware101.notes.features.allnotes.domain.extensions.toNotes
 import com.mksoftware101.notes.features.allnotes.types.Notes
 
 class FetchNotesUseCase(private val repository: NotesRepository) : NoParamUseCase<Notes> {
     override fun run(): Notes {
-        return emptyList()
+        return repository.getAllNotes().toNotes()
     }
 }
