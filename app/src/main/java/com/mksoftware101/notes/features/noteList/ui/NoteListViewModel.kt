a10fd0e4d7a060500937c1ae56fdd884dfa587da
Marek Kaszycki <marek.kaszycki@northmill.se>
2021-11-08 14:32:00 +0100

    Added AsyncDiffObservable list instead of regular one

--- app/src/main/java/com/mksoftware101/notes/features/noteList/ui/NoteListViewModel.kt
+++ app/src/main/java/com/mksoftware101/notes/features/noteList/ui/NoteListViewModel.kt
@@ -1,8 +1,8 @@
 package com.mksoftware101.notes.features.noteList.ui
 
-import androidx.databinding.ObservableArrayList
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.recyclerview.widget.DiffUtil
 import com.mksoftware101.notes.BR
 import com.mksoftware101.notes.R
 import com.mksoftware101.notes.features.noteList.domain.GetObservableNoteListUseCase
@@ -11,6 +11,7 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import me.tatarka.bindingcollectionadapter2.ItemBinding
+import me.tatarka.bindingcollectionadapter2.collections.AsyncDiffObservableList
 import javax.inject.Inject
 
 @HiltViewModel
@@ -18,17 +19,32 @@ class NoteListViewModel @Inject constructor(
     private val getNoteListUseCase: GetObservableNoteListUseCase
 ) : ViewModel() {
 
-    val displayItems = ObservableArrayList<NoteListItemViewModel>()
+    private val displayItemCallback = object : DiffUtil.ItemCallback<NoteListItemViewModel>() {
+        override fun areItemsTheSame(
+            oldItem: NoteListItemViewModel,
+            newItem: NoteListItemViewModel
+        ): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(
+            oldItem: NoteListItemViewModel,
+            newItem: NoteListItemViewModel
+        ): Boolean {
+            return oldItem.title == newItem.title &&
+                    oldItem.creationDate == newItem.creationDate
+        }
+
+    }
+
+    val displayItems = AsyncDiffObservableList(displayItemCallback)
     val displayItemBinding = ItemBinding.of<NoteListItemViewModel>(BR.vm, R.layout.item_note_list)
 
     internal fun getNoteList() {
         viewModelScope.launch {
             try {
                 getNoteListUseCase.run().collect { noteList ->
-                    with(displayItems) {
-                        clear()
-                        addAll(noteList.toDisplay())
-                    }
+                    displayItems.update(noteList.toDisplay())
                 }
             } catch (e: Exception) {
                 e.printStackTrace()
