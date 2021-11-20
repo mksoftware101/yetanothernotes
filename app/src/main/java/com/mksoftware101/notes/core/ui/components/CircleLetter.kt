c2b4b42ca1222b2fcc177e4aa74ab7ef20fcff5b
Marek Kaszycki <marek.kaszycki@northmill.se>
2021-11-20 18:31:10 +0100

    CircleLetter as separate view

---
+++ app/src/main/java/com/mksoftware101/notes/core/ui/components/CircleLetter.kt
@@ -0,0 +1,46 @@
+package com.mksoftware101.notes.core.ui.components
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.LinearLayout
+import androidx.databinding.DataBindingUtil
+import com.mksoftware101.notes.R
+import com.mksoftware101.notes.databinding.CircleLetterBinding
+
+class CircleLetter @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : LinearLayout(context, attrs, defStyleAttr) {
+
+    private val layout: CircleLetterBinding by lazy {
+        DataBindingUtil.inflate(LayoutInflater.from(context), R.layout.circle_letter, this, true)
+    }
+
+    private val lettersToColorMap = mapOf(
+        'A'..'D' to R.color.color1,
+        'E'..'H' to R.color.color2,
+        'I'..'L' to R.color.color3,
+        'M'..'P' to R.color.color4,
+        'Q'..'T' to R.color.color5,
+        'U'..'X' to R.color.color6,
+        'Y'..'Z' to R.color.color7,
+    )
+
+    init {
+        val circleLetterAttributes = context.obtainStyledAttributes(attrs, R.styleable.CircleLetter)
+        circleLetterAttributes.recycle()
+    }
+
+    fun setLetter(letterText: String) {
+        var letterColorId: Int = R.color.color_default
+        if (letterText.isNotBlank() && letterText[0].isLetter()) {
+            val letter: Char = letterText[0].toUpperCase()
+            letterColorId = lettersToColorMap.filter { it.key.contains(letter) }.map { it.value }[0]
+            layout.letterTextView.text = letter.toString()
+        }
+        layout.circleLetterLayout.backgroundTintList =
+            context.resources.getColorStateList(letterColorId)
+    }
+}
--- app/src/main/java/com/mksoftware101/notes/features/noteList/ui/NoteListItemViewModel.kt
+++ app/src/main/java/com/mksoftware101/notes/features/noteList/ui/NoteListItemViewModel.kt
@@ -5,7 +5,7 @@ import androidx.lifecycle.ViewModel
 import com.mksoftware101.notes.R
 import timber.log.Timber
 
-class NoteListItemViewModel(var title: String, var creationDate: String, var id: Int, @ColorRes val color: Int = R.color.secondary, val letter: String='A'.toString()) : ViewModel() {
+class NoteListItemViewModel(var title: String, var creationDate: String, var id: Int, @ColorRes val color: Int = R.color.secondary, val letter: String) : ViewModel() {
     fun onClick() {
         Timber.d("[d] Clicked $title")
     }
